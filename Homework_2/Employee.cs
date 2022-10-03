2e0cfbf109ca550554d691b019e15874c3252172
DiaBatMan <garsian4ik@gmail.com>
2022-10-03 11:21:20 +0300

    a

---
+++ Homework_2/Employee.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+public class Employee
+{
+    public string Name;
+    public string Surname;
+    public DateTime BirthDate;
+    public decimal Salary;
+    
+    public override string ToString()
+    {
+        string info = "\nName: "+Name+"\nSurname: "+Surname+"\nBirthDate: "+BirthDate+"\nSalary: "+Salary;
+
+        return info;
+    }
+}
---
+++ Homework_2/Factory.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+
+public class Factory
+{
+	public string Name;
+	public Employee[] employee;
+    public Products[] product;
+
+	private decimal AvgSalary 
+    {
+        get
+        {
+            decimal sum=0;
+            for (int i = 0; i < employee.Length; i++)
+            {
+                sum = sum + employee[i].Salary;
+            }
+
+            return sum / employee.Length;
+        }
+    }
+    private decimal TotalSalary 
+    { 
+        get
+        {
+            decimal sum = 0;
+
+            for (int i = 0; i < employee.Length; i++)
+            {
+                sum = sum + employee[i].Salary;
+            }
+
+            return sum;
+        }
+    }
+    private decimal GDP 
+    {
+        get
+        {
+            decimal sum = 0;
+
+            for (int i = 0; i < products.Length; i++)
+            {
+                sum = sum + products[i].Price;
+            }
+
+            return sum / employee.Length;
+
+        }
+    }
+    private decimal EmpCount 
+    {
+        get
+        {
+            return employee.Length;
+        }
+    }
+
+    public override string ToString()
+    {
+        string info = "\nName: " + Name + "\nAvgSalary: " + AvgSalary + "\nTotalSalary: " + TotalSalary + "\nGDP: " + GDP+"EmpCount: "+ EmpCount;
+
+        return info;
+    }
+}
